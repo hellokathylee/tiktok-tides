c8ef1e66545f664e4e94fa351bf49f35baf203f3
pro972 <kerrygu8@gmail.com>
2025-11-27 15:28:53 -0500

    fixed record player problems

--- src/vizzes/record-player/index.js
+++ src/vizzes/record-player/index.js
@@ -361,8 +361,8 @@ export class RecordPlayerViz {
         const ringCount = Math.max(this.data.length, 1);
         const ringStep = (OUTER_RADIUS - INNER_RADIUS) / ringCount;
         this.radiusScale = (index) => OUTER_RADIUS - (index + 0.75) * ringStep;
-        const maxAngle = 35;
-        const minAngle = 10;
+        const maxAngle = 32;
+        const minAngle = 8;
         const domainEnd = Math.max(ringCount - 1, 1);
         this.angleScale = d3.scaleLinear().domain([0, domainEnd]).range([minAngle, maxAngle]);
     }
